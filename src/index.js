146fc966ea5fd60633ac4ffe882b7854a3e8f582
poli8512 <69201867+poli8512@users.noreply.github.com>
2021-02-23 20:21:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    let n= num>0?num: Math.abs(num)
+    return +n.toString().split('').reverse().join('')
+
 }
